5e687ac5ff1e05b69d9f5c51c2c8543c18c217df
Aymane Nouhail <92037663+Aymane-Nouhail@users.noreply.github.com>
2022-04-11 03:11:05 +0000

    Update day4_level01.c

--- day4/assignments/day4_level01.c
+++ day4/assignments/day4_level01.c
@@ -36,12 +36,13 @@ void reverseString(char* s){
     }
 }
 
-/*char** reverseInPlace(char* str1){
-    char ** newString = (char **)malloc(10*sizeof(char *));
+ void str_wreverse(char * str1){
+    /*char ** newString = (char **)malloc(10*sizeof(char *));
     for(int i=0;i<10;i++)
-        newString[i]=(char *)malloc(10*sizeof(char));
-    int j=0, ctr=0;
-    for(int i=0;i<=(al_strlen(str1));i++)
+        newString[i]=(char *)malloc(10*sizeof(char));*/
+    char newString[10][10];
+    int j=0, ctr=0, i=0;
+    for(i=0;i<=(al_strlen(str1));i++)
     {
         if(str1[i]==' '||str1[i]=='\0')
         {
@@ -55,9 +56,14 @@ void reverseString(char* s){
             j++;
         }
     }
-    for(int i=0;i<10;i++) reverseString(newString[i]);
-    return newString;
-}*/
+    for(i=ctr;i<10;i++){
+        for(j=0;j<10;j++) newString[i][j] = '\0';
+    }
+    for(i=0;i<10;i++) reverseString(newString[i]);
+    for(i=0;i<10;i++) printf("%s ",newString[i]);
+    return;
+    //return newString;
+}
 
 int main(){
     char word[10];
@@ -72,38 +78,10 @@ int main(){
     al_strcat(copy,copy);
     printf("the concatenation of the previous string is : %s\n",copy);
     //next is what concerns inversing a string word by word.
-    //instead of using a function I had to do the thing in main because i cannot use malloc.
-    //please do input one extra space before inputting your word for this to work properly.
-    //this can be fixed by replacing while(str1[i] != '\0') with for(i=0;i<al_strlen(str1);i++)
     printf("Enter the string to inverse word by word : \n");
-    char str1[100];
-    char newString[10][10]; 
-    int i=0,j,ctr;
-    gets(str1);
-    j=0; ctr=0;
-    while(str1[i] != '\0')
-    {
-        if(str1[i]==' '||str1[i]=='\0')
-        {
-            newString[ctr][j]='\0';
-            ctr++;
-            j=0;
-        }
-        else
-        {
-            newString[ctr][j]=str1[i];
-            j++;
-        }
-        i++;
-    }
-    for(i=ctr;i<10;i++){
-        for(j=0;j<10;j++) newString[i][j] = '\0';
-    }
-    for(i=0;i<10;i++) reverseString(newString[i]);
-    /*char str1[100];
+    char str1[50];
     gets(str1);
     char newString[10][10];
-    printf("%s",reverseInPlace(str1)[1]);*/
-    for(i=0;i<10;i++) printf("%s ",newString[i]);
+    str_wreverse(str1);
     return 0;
 }
