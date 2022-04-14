51869d79ee7030d86896fcb07d1762e19fe62ea7
Aymane-Nouhail <Aymane.Nouhail@gmail.com>
2022-04-14 01:57:43 +0000

    Update day3_exo3.c

--- day3/assignments/day3_exo3.c
+++ day3/assignments/day3_exo3.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 
 struct contact
 {
     int phoneNumber;
     int id;
-    char fullName[10];
+    char fullName[20];
 };
 
 typedef struct contact contact;
 
 int main(){
-    int userPick=0, order=0;
+    int userPick=0, order=0, i ; char c;
     contact book[8];
     printf("\t Choose one of the following by typing in the corresponding number: \n----------------------------------\n");
     printf("\t1. Add a contact.\n");
@@ -20,20 +22,28 @@ int main(){
     while(userPick != 3){
     scanf("%d",&userPick);
         if(userPick == 1){
-            char* temp;
+            /*this part is just to be able to scan a string with spaces, couldn't get "gets" and "fgets" to work 
+            so i used this section of code from https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf*/
             printf("\tEnter the full name : ");
             scanf("%s",book[order].fullName);
-            //fgets(book[order].fullName, 10, stdin);
+            i = strlen(book[order].fullName);
+            do
+            {
+                scanf("%c", &c);
+                (book[order].fullName)[i++] = c;
+            } while (c != '\n');
+            (book[order].fullName)[i - 1] = 0;
+            //////section of scanning the name ends here.
             printf("\t----------------------------------\n");
             printf("\tEnter the phone number : ");
             scanf("%d",&book[order].phoneNumber);
             printf("----------------------------------\n");
-            book[order].id=order;;
+            book[order].id=order;
             order++;
         }
         if(userPick == 2){
             printf("\n----------------------------------All contacts : ----------------------------------\n");
-            for(int i=0;i<order;i++){
+            for(i=0;i<order;i++){
                 printf("\tfull name : %s \n",book[i].fullName);
                 printf("\tphone number : %d \n",book[i].phoneNumber);
                 printf("\tid : %d\n",book[i].id);
